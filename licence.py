2f7d6e53235199efa275cb585658b1dc292ee51c
miscing <miscing-public@protonmail.com>
2020-04-24 00:03:45 +0300

    removed space symbols from otherwise empty lines

--- licence.py
+++ licence.py
@@ -7,16 +7,16 @@ import argparse
 
 #  templates
 euplTop = """Copyright %d %s
-                                                               
+
  Licensed under the EUPL, Version 1.2 or – as soon they
 will be approved by the European Commission - subsequent
 versions of the EUPL (the "Licence");
  You may not use this work except in compliance with the
 Licence.
  You may obtain a copy of the Licence at:
-                                                               
+
  https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
-                                                               
+
  Unless required by applicable law or agreed to in
 writing, software distributed under the Licence is
 distributed on an "AS IS" basis,
@@ -413,7 +413,10 @@ def comment_out(text, comment):
     res = []
     res.append(comment)
     for line in text.splitlines():
-        res.append(comment+' '+line)
+        if line == "":
+            res.append(comment)
+        else:
+            res.append(comment+' '+line)
     res.append(comment)
     res.append("")
     return "\n".join(res)
